c3bb810cd8fd6847edb855ddcd60e6c3face78ec
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-13 19:38:50 +0200

    Use g++ of travis

--- retrocalclib/Debug/subdir.mk
+++ retrocalclib/Debug/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	$(CXX) -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
